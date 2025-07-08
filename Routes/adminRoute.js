7dfb57e585f8d37515b7e51cd93c2054aeeeff5d
vikram-42-alla <allatrivikram@gmail.com>
2025-07-08 11:10:50 +0530

    commit

--- Routes/adminRoute.js
+++ Routes/adminRoute.js
@@ -3,6 +3,8 @@ const express=require("express")
 const app=express()
 const jwt=require("jsonwebtoken")
 const bcrypt=require("bcryptjs")
+const cors=require("cors")
+app.use(cors())
 app.use(express.json())
 app.post("/register",async(req,res)=>{
     try {
